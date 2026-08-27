0d73159fb05381ecc3166c3b0c3c836293f6c5ef
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-08-27 23:21:35 +0530

    expanded state of folder

--- src/components/FileExplorer.jsx
+++ src/components/FileExplorer.jsx
@@ -2,13 +2,37 @@ import React, { useState } from "react";
 import { fileData } from "./data/explorerData";
 
 const List = ({ data }) => {
+  const [isExpanded, setIsExpanded] = useState({});
+  const handleExpand = (item) => {
+    setIsExpanded((prev) => ({
+      ...prev,
+      [item.name]: !prev[item.name],
+    }));
+  };
   return (
     <div className="flex flex-col items-start text-start mx-6 p-3">
       {data.map((item) => {
         return (
-          <div>
-            <span>{item.name}</span>
-            {item?.children && <List data={item.children} />}
+          <div className="m-2" key={item.id}>
+            {item.isFolder ? (
+              !isExpanded?.[item.name] ? (
+                <span>📁</span>
+              ) : (
+                <span>📂</span>
+              )
+            ) : (
+              <span>🖺</span>
+            )}
+
+            <span
+              className="cursor-pointer bg-gray-200 rounded-lg p-2"
+              onClick={() => handleExpand(item)}
+            >
+              {item.name}
+            </span>
+            {isExpanded?.[item.name] && item?.children && (
+              <List data={item.children} />
+            )}
           </div>
         );
       })}
